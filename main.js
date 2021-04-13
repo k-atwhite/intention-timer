11a45674405c6cff949152571726303ca95b6d8a
Erica <>
2021-04-13 16:13:07 -0600

    Create function to change color button

---
+++ main.js
@@ -0,0 +1,11 @@
+var btnSection = document.querySelector('.btn-section');
+var studyBtn = document.querySelector('#study-btn');
+var meditateBtn = document.querySelector('#meditate-btn');
+var exerciseBtn = document.querySelector('#exercise-btn');
+
+
+// btnSection.addEventListener("click", function(event) {
+
+function changeButtonColor(button, cssClass) {
+  button.classList.add(cssClass);
+}
