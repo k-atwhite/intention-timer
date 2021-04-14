18bd44efba9183c5b4ead54203e1397b4f1af795
Erica <>
2021-04-14 14:49:38 -0600

    Add button image variables in main js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var btnSection = document.querySelector('.btn-section');
+// Activity Btns
 var studyBtn = document.querySelector('#study-btn');
 var meditateBtn = document.querySelector('#meditate-btn');
 var exerciseBtn = document.querySelector('#exercise-btn');
+//Button Imgs
+var studyImg = document.querySelector('#study-img');
+var meditateImg = document.querySelector('#meditate-img');
+var exerciseImg = document.querySelector('#exercise-img');
 
 
 // btnSection.addEventListener("click", function(event) {
